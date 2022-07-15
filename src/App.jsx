f934046e0f21d21eb9683a59590e9763a7f9645c
shahil-leo <shahilcode@gmail.com>
2022-07-15 15:36:35 +0530

    This is for all the navbar home page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbars from './components/Navbars'
+import { Cart } from './pages/Cart'
+import Home from './pages/Home'
+import { Login } from './pages/Login'
+import Product from "./pages/Product"
+import { Register } from './pages/Register'
+function App() {
+  return (
+    <div>
+    <Home/>
+  
+    </div>
+  )
+}
+
+export default App
