0afbc8341b8b3a433a3c78f4d4c784622279b10d
shahil-leo <shahilcode@gmail.com>
2022-07-15 17:22:52 +0530

    Data section added

---
+++ src/data.js
@@ -0,0 +1,99 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:"https://images.unsplash.com/photo-1444460381791-02e0621c12f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        title:"SUMMER SALE",
+        desc:"DONT COMPARE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg:'f5fafd,'
+    },
+    {   
+        id:2,
+        img:"https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+        title:"AUTOMN COLLECTION",
+        desc:"DONT COMPARE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg:'f5fafd',
+    },
+    {
+        id:3,
+        img:"https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg",
+        title:"WINTER COLLECTION",
+        desc:"DONT COMPARE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg:'f5fafd',
+    },
+];
+export const categories=[
+    {
+        id:1,
+        img:"https://images.unsplash.com/photo-1444460381791-02e0621c12f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        title:"SUMMER SALE",
+        
+    },
+    {   
+        id:2,
+        img:"https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+        title:"AUTOMN COLLECTION",
+      
+    },
+    {
+        id:3,
+        img:"https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg",
+        title:"WINTER COLLECTION",
+       
+    },
+];
+export const popularProducts=[
+    {
+        id:1,
+        img:"https://images.unsplash.com/photo-1444460381791-02e0621c12f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+       
+        
+    },
+    {   
+        id:2,
+        img:"https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+       
+      
+    },
+    {
+        id:3,
+        img:"https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg",
+      
+       
+    },
+    {   
+        id:4,
+        img:"https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+       
+      
+    },
+    {
+        id:5,
+        img:"https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg",
+      
+       
+    },
+    {
+        id:6,
+        img:"https://images.unsplash.com/photo-1444460381791-02e0621c12f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+       
+        
+    },
+    {   
+        id:7,
+        img:"https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+       
+      
+    },
+    {
+        id:8,
+        img:"https://images.unsplash.com/photo-1444460381791-02e0621c12f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+       
+        
+    },
+    {   
+        id:9,
+        img:"https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+       
+      
+    },
+];
