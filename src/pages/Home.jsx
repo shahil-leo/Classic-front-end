5ba41f73149b9ea5fe50075f5247837b54d1c374
shahil-leo <shahilcode@gmail.com>
2022-07-15 15:43:21 +0530

    Pages are added the home page cart product etc

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Categories from "../components/Categories"
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newletter from '../components/Newletter'
+import Products from '../components/Products'
+import Slider from "../components/Slider"
+
+
+const Home=() => {
+  return (
+    <div>
+        <Announcement/>
+        <Navbar/>
+        <Slider/>
+        <Categories/>
+        <Products/>
+        <Newletter/>
+        <Footer/>
+
+    </div>
+  )
+}
+
+export default Home
